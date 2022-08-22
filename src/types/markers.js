25932819b4ddb3b78ddfa9c61db6aa8b15723fa4
Johannes Bechberger <johannes.bechberger@sap.com>
2022-08-22 13:16:27 +0200

    Implement "showByDefault" track config

--- src/types/markers.js
+++ src/types/markers.js
@@ -85,6 +85,7 @@ export type MarkerTrackConfig = {|
   tooltip?: string,
   // height of the track in pixels
   height?: MarkerTrackConfigLineHeight,
+  showByDefault?: boolean,
   lines: Array<{|
     key: string,
     fillColor?: string,
